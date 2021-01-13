5857967e9419d14c8f4e6df14e4c44093a4b91b6
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-13 17:27:32 -0500

    commit tree 7a494ce063ce283c4c7df9ffdf9b5c59376c8b11

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+    if array.length == 0
+        puts "The line is currently empty."
+    else
+        string = ""
+        i = 0
+        while (i < array.length)  
+          string << " #{i+1}. #{array[i]}"
+          i+=1  
+        end
+        puts "The line is currently:" + string
+    end
+end
+
+
+def take_a_number(katz_deli, patron)
+    katz_deli << patron
+    puts "Welcome, #{patron}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli[0]}."
+        katz_deli.shift
+    end
+end
